8c11686ae2a4ad81bdc3449f82719e61ba075e1a
Jhonatan <jhonjhonpf@hotmail.com>
2023-08-31 20:14:15 -0300

    Entrar

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 import Menu from './components/Menu'
+import Entrar from './pages/Entrar'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
 
-    <Menu/>
+    <Entrar/>
     
   </React.StrictMode>
 )
