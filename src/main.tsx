c7b4daca8d73aa84f16c83c8f00805594ff10248
Marcel de Lima Brito <marceldrum85@gmail.com>
2023-09-01 19:36:56 -0300

    componente projeto cadastro

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,20 @@ import './index.css'
 
 import Menu from './components/Menu'
 import Entrar from './pages/Entrar'
-
-
+import ProjetoCadastro from './pages/ProjetoCadastro'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <BrowserRouter>
 
-    <Entrar/>
+      <Routes>
+
+        <Route path="/cadastrar/projeto" element={<ProjetoCadastro/>} />
+
+      </Routes>
+
+    </BrowserRouter>
     
+
   </React.StrictMode>
 )
