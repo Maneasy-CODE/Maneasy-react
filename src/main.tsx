d8448954f14479ca4d4b5ba87b33b41a1917efc7
Lucas Oliveira <lucas.gon.oliv@gmail.com>
2023-09-04 21:15:15 -0300

    adicionado o componente de botão

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,9 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 import Menu from './components/Menu'
-import Entrar from './pages/Entrar'
-
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
 
-    <Entrar/>
-    
   </React.StrictMode>
 )
