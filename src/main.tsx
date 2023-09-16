eab8895e4cafb136bd87fcd1845734ded7ba26d1
Priscila Laurentino <priscila_lau@yahoo.com.br>
2023-09-15 21:14:11 -0300

    CSS e HTML duplicado de cadastro

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+//estilização
 import './index.css'
 
-import Menu from './components/Menu'
-import Entrar from './pages/Entrar'
+//pagina
+/*import ProfissionalCadastro from "./pages/ProfissionalCadastro"*/
+/*import ProfissionalConsulta from "./pages/ProfissionalConsulta"*/
+import ProfissionalAtualizar from "./pages/ProfissionalAtualizar"
 
-
+//rotas
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-
-    <Entrar/>
-    
-  </React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+      { /* <Route path='/' element={<ProfissionalCadastro />} />
+        <Route path='/profissionalCadastro' element={<ProfissionalCadastro />} />*/}
+        {/*<Route path='/' element={<ProfissionalConsulta />} />*/}
+        { /*<Route path='/profissionalConsulta' element={<ProfissionalConsulta />} />*/}
+        <Route path='/' element={<ProfissionalAtualizar />} />
+        { /*<Route path='/ProfissionalAtualizar' element={<ProfissionalAtualizar />} />*/}
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
