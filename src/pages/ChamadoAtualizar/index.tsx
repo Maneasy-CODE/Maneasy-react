134b844bca0ae0a86d05ebd294bdbe80eb12d172
Gustavo <gustavoluizmiranda405@gmail.com>
2023-09-18 20:37:43 -0300

    required e form

--- src/pages/ChamadoAtualizar/index.tsx
+++ src/pages/ChamadoAtualizar/index.tsx
@@ -28,118 +28,121 @@ function ChamadoAtualizar() {
                             <img src={IconTitulo} alt="" />
                             <h1>Demandas</h1>
                         </div>
-                        <div className="buttons-grid">
+                        <nav className="buttons-grid">
                             <Link className="button-cadastrar" to={""}>Atualizar</Link>
                             <Link className="button-consultar" to={""}>Consultar</Link>
-                        </div>
-                        <div className="demanda">
-                            <span>Nome da Demanda</span>
-                            <div className="demanda-cadastrar input-icons">
-                                <input className="input-field" name="nome-demanda" type="text" />
-                                <Icon className="icon" icon="mdi:clipboard-edit-outline" />
-                            </div>
-                        </div>
-                        <div className="datas">
-                            <div className="criacao">
-                                <span>Criação</span>
-                                <div className="data-conte input-icons">
 
-                                    <Icon className="icon" icon="bx:calendar" />
-
-                                    <input className="input-field" name="criacao" type="date" />
+                        </nav>
+                        <form action="">
+                            <div className="demanda">
+                                <span>Nome da Demanda</span>
+                                <div className="demanda-cadastrar input-icons">
+                                    <input className="input-field" name="nome-demanda" type="text" />
+                                    <Icon className="icon" icon="mdi:clipboard-edit-outline" />
                                 </div>
                             </div>
-                            <div className="inicio">
-                                <span>Ínicio</span>
-                                <div className="data-conte input-icons">
+                            <div className="datas">
+                                <div className="criacao">
+                                    <span>Criação</span>
+                                    <div className="data-conte input-icons">
 
-                                    <Icon className="icon" icon="bx:calendar" />
+                                        <Icon className="icon" icon="bx:calendar" />
 
-                                    <input className="input-field" name="criacao" type="date" />
+                                        <input className="input-field" name="criacao" type="date" />
+                                    </div>
+                                </div>
+                                <div className="inicio">
+                                    <span>Ínicio</span>
+                                    <div className="data-conte input-icons">
+
+                                        <Icon className="icon" icon="bx:calendar" />
+
+                                        <input className="input-field" name="criacao" type="date" />
+                                    </div>
+                                </div>
+                                <div className="termino">
+                                    <span>Término</span>
+                                    <div className="data-conte input-icons">
+
+                                        <Icon className="icon" icon="bx:calendar" />
+
+                                        <input className="input-field" name="criacao" type="date" />
+                                    </div>
                                 </div>
                             </div>
-                            <div className="termino">
-                                <span>Término</span>
-                                <div className="data-conte input-icons">
-
-                                    <Icon className="icon" icon="bx:calendar" />
-
-                                    <input className="input-field" name="criacao" type="date" />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="objetivo-demanda">
                             <div className="objetivo-demanda">
-                                <span>Objetivo demanda</span>
-                                <textarea
-                                    className="objetivo-form"
-                                    spellCheck="false"
-                                    name=""
-                                    id=""
-                                    cols={30}
-                                    rows={10}
-                                    defaultValue={
-                                        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas commodi in qui ex provident minima repellendus nostrum odio quos ut asperiores repudiandae placeat distinctio, maiores corrupti ullam fuga nam optio."
-                                    }
-                                />
+                                <div className="objetivo-demanda">
+                                    <span>Objetivo demanda</span>
+                                    <textarea
+                                        className="objetivo-form"
+                                        spellCheck="false"
+                                        name=""
+                                        id=""
+                                        cols={30}
+                                        rows={10}
+                                        defaultValue={
+                                            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas commodi in qui ex provident minima repellendus nostrum odio quos ut asperiores repudiandae placeat distinctio, maiores corrupti ullam fuga nam optio."
+                                        }
+                                    />
+                                </div>
+                                <div id="barra-rolamento" />
                             </div>
-                            <div id="barra-rolamento" />
-                        </div>
-                        <div className="objetivo-demanda">
                             <div className="objetivo-demanda">
-                                <span>Consultoria na demanda</span>
-                                <textarea
-                                    className="objetivo-form"
-                                    spellCheck="false"
-                                    name=""
-                                    id=""
-                                    cols={30}
-                                    rows={10}
-                                    defaultValue={
-                                        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, cupiditate. Quo iste magnam nostrum adipisci! Odit suscipit sit laudantium. Perspiciatis rem, deleniti quas libero ipsa officiis corporis laboriosam temporibus quo."
-                                    }
-                                />
+                                <div className="objetivo-demanda">
+                                    <span>Consultoria na demanda</span>
+                                    <textarea
+                                        className="objetivo-form"
+                                        spellCheck="false"
+                                        name=""
+                                        id=""
+                                        cols={30}
+                                        rows={10}
+                                        defaultValue={
+                                            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, cupiditate. Quo iste magnam nostrum adipisci! Odit suscipit sit laudantium. Perspiciatis rem, deleniti quas libero ipsa officiis corporis laboriosam temporibus quo."
+                                        }
+                                    />
+                                </div>
+                                <div id="barra-rolamento" />
                             </div>
-                            <div id="barra-rolamento" />
-                        </div>
-                        <div className="progresso-demanda">
-                            <div className="orcamento-demanda">
-                                <span>Orçamento de demanda</span>
-                                <div className="data-conte input-icons">
+                            <div className="progresso-demanda">
+                                <div className="orcamento-demanda">
+                                    <span>Orçamento de demanda</span>
+                                    <div className="data-conte input-icons">
 
-                                    <Icon className="icon" icon="fa:dollar" />
+                                        <Icon className="icon" icon="fa:dollar" />
 
-                                    <input className="input-field" name="criacao" type="text" />
+                                        <input className="input-field" name="criacao" type="text" />
+                                    </div>
+                                </div>
+                                <div className="tempo-demanda">
+                                    <span>Tempo de demanda</span>
+                                    <div className="data-conte input-icons">
+
+                                        <Icon className="icon" icon="ps:clock" />
+
+                                        <input className="input-field" name="criacao" type="text" />
+                                    </div>
+                                </div>
+                                <div className="select_status">
+                                    <span>Status da Demanda</span>
+                                    <div className="select-icons">
+
+                                        <Icon className="icon_select" icon="el:ok-sign" />
+
+                                        <select className="data-conte" name="status" id="">
+                                            <option value="">Aberto</option>
+                                            <option value="">Em andamento</option>
+                                            <option value="">Finalizado</option>
+                                        </select>
+                                    </div>
                                 </div>
                             </div>
-                            <div className="tempo-demanda">
-                                <span>Tempo de demanda</span>
-                                <div className="data-conte input-icons">
-
-                                    <Icon className="icon" icon="ps:clock" />
-
-                                    <input className="input-field" name="criacao" type="text" />
-                                </div>
-                            </div>
-                            <div className="select_status">
-                                <span>Status da Demanda</span>
-                                <div className="select-icons">
-
-                                    <Icon className="icon_select" icon="el:ok-sign" />
-
-                                    <select className="data-conte" name="status" id="">
-                                        <option value="">Aberto</option>
-                                        <option value="">Em andamento</option>
-                                        <option value="">Finalizado</option>
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
+                        </form>
                         <Btn_submit_cancel_squad />
                     </div>
                 </div>
             </section>
-        </main>
+        </main >
 
     )
 }
