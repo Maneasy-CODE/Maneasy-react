7257173c816a12ee0f3ff11fe08551347dacc9f6
Lucas Oliveira <lucas.gon.oliv@gmail.com>
2023-09-19 20:07:24 -0300

    finalizado merge cadastro

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 //IMPORT PAGINAS
 import Menu from './components/Menu'
 import Entrar from './pages/Entrar';
+import Cadastro from './pages/Cadastro';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>   {/*// INDICA QUE A APLICAÇÃO TERA ROTAS  */}
       <Routes>         {/*// INDICA QUE TEM UMA LISTA DE COMPONENTES, OU SEJA, UMA LISTA DE ROTAS  */}
         <Route path='/pagina/entrar' element={<Entrar />} />  //   {/*INDICA O CAMINHO DO COMPONENTE E O NOME DA ROTA DELE */}
+        <Route path='/pagina/cadastro' element={<Cadastro />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>,)
