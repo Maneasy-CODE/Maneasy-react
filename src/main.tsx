1793a01ced9872e7381061b390412565223051f4
Priscila Laurentino <priscila_lau@yahoo.com.br>
2023-09-18 21:52:45 -0300

    espacamentos

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route />
 
-        <Route path="/atualizar/projeto" element={<ProjetoAtualizar />} />
+        <Route path="/" element={<ProjetoAtualizar />} />
 
       </Routes>
 
