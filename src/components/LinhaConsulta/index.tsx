1033ac29471d9c9ae0aa623910ced0ed5ba13110
Jhonatan <jhonjhonpf@hotmail.com>
2024-02-01 21:45:03 -0300

    Atualizacao

--- src/components/LinhaConsulta/index.tsx
+++ src/components/LinhaConsulta/index.tsx
@@ -6,19 +6,10 @@ import { Link } from 'react-router-dom';
 function Lista_consulta(props: any) {
     return (
         <tr id="consulta" className='consulta_lista'>
-            <td className="linha_consultas_id">{props.id}</td>
-            <td className="linha_consultas_tipo">{props.nome}</td>
-            <select className="linha_consultas_status">
-                <option value="ativos">Ativos</option>
-                <option value="finalizados">Finalizados</option>
-                <option value="todos">Todos</option>
-            </select>
-            <td className='button_consulta'>
-                {" "}
-                <Link to={"#"}>
-                    <img src={impressora} />
-                </Link>
-            </td>
+            <td className="linha_consultas_nome_profissional">{props.nome_profissional}</td>
+            <td className="linha_consultas_nome_projeto">{props.nome_projeto}</td>
+            <td className="linha_consultas_tipo">{props.tipo}</td>
+            <td className="linha_consultas_status">{props.status}</td>
         </tr>
     );
 }
--- src/pages/Consultas/index.tsx
+++ src/pages/Consultas/index.tsx
@@ -15,48 +15,35 @@ import { Link } from "react-router-dom";
 function Consultas() {
     const listaConsulta: any = [
         {
-            id: 1010,
-            nome: "Projeto"
+            nome_profissional: "Nome Profissional Exemplo",
+            nome_projeto: "Nome Projeto Exemplo",
+            tipo: "Projeto",
+            status: "Em andamento"
+
         },
+
         {
-            id: 1010,
-            nome: "Projeto"
+            nome_profissional: "Nome Profissional Exemplo",
+            nome_projeto: "Nome Demanda Exemplo",
+            tipo: "Demanda",
+            status: "Em andamento"
+
         },
+
         {
-            id: 1010,
-            nome: "Projeto"
+            nome_profissional: "Nome Profissional Exemplo",
+            nome_projeto: "Nome Chamado Exemplo",
+            tipo: "Chamado",
+            status: "Em andamento"
+
         },
+
         {
-            id: 1010,
-            nome: "Projeto"
-        },
-        {
-            id: 1010,
-            nome: "Projeto"
-        },
-        {
-            id: 1010,
-            nome: "Projeto"
-        },
-        {
-            id: 1010,
-            nome: "Projeto"
-        },
-        {
-            id: 1010,
-            nome: "Projeto"
-        },
-        {
-            id: 1010,
-            nome: "Projeto"
-        },
-        {
-            id: 1010,
-            nome: "Projeto"
-        },
-        {
-            id: 1010,
-            nome: "Projeto"
+            nome_profissional: "Nome Profissional Exemplo",
+            nome_projeto: "-",
+            tipo: "-",
+            status: "-"
+
         },
     ]
     return (
@@ -69,20 +56,47 @@ function Consultas() {
                             <img src={bolinha_titulo} alt="" />
                             <h1>Consultas</h1>
                         </div>
-                        <div className="section_ID_Consultar">
-                            <div className="input_nome">
-                                <label htmlFor="Pesquisa">Pesquisa</label>
-                                <div className="input-icons">
-                                    <input name="Pesquisa" className="input-field" type="text" placeholder="Digite o nome do chamado ou ID" required />
+
+                        <div className="pesquisa_and_filtro">
+
+                            <div className="section_ID_Consultar">
+                                <div className="input_nome">
+                                    <label htmlFor="Pesquisa">Pesquisa</label>
+                                    <div className="input-icons">
+                                        <input name="Pesquisa" className="input-field" type="text" placeholder="Digite o nome do chamado ou ID" required />
+                                    </div>
                                 </div>
+                                <Link to={"#"} className="section_pesquisa_btn">
+                                    <img src={lupa} alt="" />
+                                </Link>
                             </div>
-                            <Link to={"#"} className="section_pesquisa_btn">
-                                <img src={lupa} alt="" />
-                            </Link>
+
+
+
+
+                            <div className="pesquisa_tipo">
+                                <label htmlFor="filtro" className="filtro">Filtro</label>
+                                <select name="Pesquisa">
+                                    <option value="projeto" className="input-field">Projeto</option>
+                                    <option value="demanda" className="input-field">Demanda</option>
+                                    <option value="demanda" className="input-field" >Chamado</option>
+                                    <option value="disponivel" className="input-field">Disponível</option>
+                                </select>
+                            </div>
+
                         </div>
 
                         <section className="section_table">
                             <h2>Consultas</h2>
+
+
+                            <div className="titulos_consulta">
+                                <p className="linha_consultas_nome_profissional">Nome do Profissional</p>
+                                <p className="linha_consultas_nome_projeto">Nome do Projeto</p>
+                                <p className="linha_consultas_status">Tipo</p>
+                                <p className="linha_consultas_status">Status</p>
+                            </div>
+
                             <div className="tabela_scroll">
                                 <table>
                                     <tbody>
@@ -90,8 +104,11 @@ function Consultas() {
                                             listaConsulta.map((consulta: any) => {
                                                 return <tr key={consulta.id}>
                                                     <Lista_consulta
-                                                        id={consulta.id}
-                                                        nome={consulta.nome}
+                                                        nome_profissional={consulta.nome_profissional}
+                                                        nome_projeto={consulta.nome_projeto}
+                                                        tipo={consulta.tipo}
+                                                        status={consulta.status}
+
                                                     />
                                                 </tr>
                                             })
