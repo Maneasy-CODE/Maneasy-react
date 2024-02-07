47a39d80686afa72cffd9a9194055b29c43dd0aa
Hosmairys <holderhosmairys@gmail.com>
2024-02-06 21:37:44 -0300

    Atualização squad montar responsividade

--- src/pages/Squad_montar/index.tsx
+++ src/pages/Squad_montar/index.tsx
@@ -15,47 +15,49 @@ import { Link } from "react-router-dom";
 
 function Squadmontar() {
     return (
-        <main id="squad_cadastro" className="container_squad_cadastro">
+        <main className="squad_montar">
             <Menu></Menu>
             <section className="container">
+
                 <div className="section_titulo">
                     <img src={bolinha_titulo} alt="" />
                     <h1>Squad</h1>
                 </div>
+
                 <form>
+
                     <div className="form_id">
                         <div className="section_pesquisa_resultado_input">
                             <label htmlFor="id">ID</label>
                             <div className="input-icons">
                                 <Icon className="icon" icon="ic:outline-badge" />
-                                <input name="id" className="input-field" type="number" placeholder="Digite o ID do squad" />
+                                <input name="id" className="input-field" type="number" />
                             </div>
                         </div>
                         <div>
-                            <Link to={"#"} className="section_cadastro_squad_btn">
-                                <img src={lupa} alt="" />
-                            </Link>
+                            <Link to={"#"} className="section_cadastro_squad_btn"><img src={lupa}
+                                alt=""/></Link>
+
                         </div>
                     </div>
+
                     <div className="section_pesquisa">
                         <div className="section_pesquisa_resultado_input">
                             <label htmlFor="nome_projeto">Nome do Projeto</label>
                             <div className="input-icons">
                                 <Icon className="icon" icon="la:project-diagram" />
-                                <input name="nome_projeto" className="input-field" type="text" placeholder="Digite o nome do projeto" />
+                                <input name="nome_projeto" className="input-field" type="text" />
                             </div>
                         </div>
                     </div>
+
                     <div className="form_date">
                         <div className="form_criacao_inicio_termino">
                             <div className="form_date_criacao_inicio_termino">
                                 <label htmlFor="criacao">Criação</label>
                                 <div className="input-icons">
-                                    <Icon
-                                        className="icon icon_calendario"
-                                        icon="zondicons:calendar"
-                                    />
-                                    <input name="criacao" className="input-field" type="date" required />
+                                    <Icon className="icon icon_calendario" icon="zondicons:calendar" />
+                                    <input name="criacao" className="input-field" type="date" />
                                 </div>
                             </div>
                         </div>
@@ -63,11 +65,8 @@ function Squadmontar() {
                             <div className="form_date_criacao_inicio_termino">
                                 <label htmlFor="inicio">Ínicio</label>
                                 <div className="input-icons">
-                                    <Icon
-                                        className="icon icon_calendario"
-                                        icon="zondicons:calendar"
-                                    />
-                                    <input name="inicio" className="input-field" type="date" required />
+                                    <Icon className="icon icon_calendario" icon="zondicons:calendar" />
+                                    <input name="inicio" className="input-field" type="date" />
                                 </div>
                             </div>
                         </div>
@@ -75,126 +74,123 @@ function Squadmontar() {
                             <div className="form_date_criacao_inicio_termino">
                                 <label htmlFor="termino">Término</label>
                                 <div className="input-icons">
-                                    <Icon
-                                        className="icon icon_calendario"
-                                        icon="zondicons:calendar"
-                                    />
-                                    <input name="termino" className="input-field" type="date" required />
+                                    <Icon className="icon icon_calendario" icon="zondicons:calendar" />
+                                    <input name="termino" className="input-field" type="date" />
                                 </div>
                             </div>
                         </div>
                     </div>
+
                     <div className="section_objetivo_projeto">
                         <div className="section_objetivo_projeto_label_textarea">
                             <label htmlFor="objetivo_projeto">Objetivo do Projeto</label>
                             <div>
-                                <textarea
-                                    id="objetivo_projeto"
-                                    name="objetivo_projeto"
-                                    rows={4}
-                                    cols={50}
-                                    defaultValue={""}
-                                />
+                                <textarea id="objetivo_projeto" name="objetivo_projeto" rows={4} cols={50}></textarea>
                             </div>
                         </div>
+
                     </div>
                     <h2 className="titulo_lista_prof">Lista de profissionais</h2>
                     <div className="section_pesquisa espacamento">
-                        {/* <div> */}
                         <div className="section_pesquisa_resultado_input">
-                            <label htmlFor="Consultar">Pesquisa</label>
+                            <label htmlFor="Pesquisa">Pesquisa</label>
                             <div className="input-icons">
-                                <Icon className="icon" icon="mingcute:user-4-fill" />
-                                <input name="Consultar" className="input-field" type="text" placeholder="Pesquise pelo nome o chapa" />
+                                <Icon className="icon" icon="ic:outline-badge" />
+                                <input name="Pesquisa" className="input-field" type="number" placeholder="Pesquise pelo nome o chapa" />
                             </div>
                         </div>
-                        <Link to={"#"} className="section_pesquisa_btn">
-                            <img src={lupa} alt="" />
-                        </Link>
-                        {/* </div> */}
+
+                        <Link to={"#"} className="section_pesquisa_btn"><img src={lupa} alt=""/></Link>
+
                     </div>
                     <div className="section_resultado">
                         <div className="tabela_scroll">
                             <div className="section_resultado_dados">
                                 <div>
                                     <p className="section_resultado_dados_id">010101</p>
-                                    <p className="section_resultado_dados_nome">
-                                        Claudio Francisco Porto
-                                    </p>
+
+                                    <p className="section_resultado_dados_nome">Claudio Francisco Porto</p>
                                 </div>
-                                <Link to={"#"} className="section_resultado_dados_btn">
-                                    <img src={btn_confirmar} alt="" />
+
+                                <Link to={""} className="section_resultado_dados_btn"><img src={btn_confirmar}
+                                    alt=""/>
                                 </Link>
                             </div>
                             <div className="section_resultado_dados">
                                 <div>
                                     <p className="section_resultado_dados_id">010102</p>
-                                    <p className="section_resultado_dados_nome">
-                                        Gustavo Luiz Miranda
-                                    </p>
+
+                                    <p className="section_resultado_dados_nome">Gustavo Luiz Miranda</p>
                                 </div>
-                                <Link to={"#"} className="section_resultado_dados_btn">
-                                    <img src={btn_confirmar} alt="" />
+
+                                <Link to={""} className="section_resultado_dados_btn"><img src={btn_confirmar}
+                                    alt=""/>
                                 </Link>
                             </div>
                             <div className="section_resultado_dados">
                                 <div>
                                     <p className="section_resultado_dados_id">010103</p>
-                                    <p className="section_resultado_dados_nome">
-                                        Hosmairys Yuriannys Holder Rodriguez
-                                    </p>
+
+                                    <p className="section_resultado_dados_nome">Hosmairys Yuriannys Holder Rodriguez</p>
                                 </div>
-                                <Link to={"#"} className="section_resultado_dados_btn">
-                                    <img src={btn_squad} alt="" />
+
+                                <Link to={""} className="section_resultado_dados_btn"><img src={btn_squad}
+                                    alt=""/>
                                 </Link>
                             </div>
                             <div className="section_resultado_dados">
                                 <div>
                                     <p className="section_resultado_dados_id">010104</p>
-                                    <p className="section_resultado_dados_nome">
-                                        Jhonatan Pereira Ferreira
-                                    </p>
+
+                                    <p className="section_resultado_dados_nome">Jhonatan Pereira Ferreira</p>
                                 </div>
-                                <Link to={"#"} className="section_resultado_dados_btn">
-                                    <img src={btn_squad} alt="" />
+
+                                <Link to={""} className="section_resultado_dados_btn"><img src={btn_squad}
+                                    alt=""/>
                                 </Link>
                             </div>
                             <div className="section_resultado_dados">
                                 <div>
                                     <p className="section_resultado_dados_id">010105</p>
+
                                     <p className="section_resultado_dados_nome">Lucas Oliveira</p>
                                 </div>
-                                <Link to={"#"} className="section_resultado_dados_btn">
-                                    <img src={btn_squad} alt="" />
+
+                                <Link to={""} className="section_resultado_dados_btn"><img src={btn_squad}
+                                    alt=""/>
                                 </Link>
                             </div>
                             <div className="section_resultado_dados">
                                 <div>
                                     <p className="section_resultado_dados_id">010106</p>
-                                    <p className="section_resultado_dados_nome">
-                                        Priscila Laurentino
-                                    </p>
+
+                                    <p className="section_resultado_dados_nome">Priscila Laurentino</p>
                                 </div>
-                                <Link to={"#"} className="section_resultado_dados_btn">
-                                    <img src={btn_squad} alt="" />
+
+                                <Link to={""} className="section_resultado_dados_btn"><img src={btn_squad}
+                                    alt=""/>
                                 </Link>
                             </div>
                             <div className="section_resultado_dados">
                                 <div>
                                     <p className="section_resultado_dados_id">010107</p>
-                                    <p className="section_resultado_dados_nome">
-                                        Marcel de Lima Brito
-                                    </p>
+
+                                    <p className="section_resultado_dados_nome">Marcel de Lima Brito</p>
                                 </div>
-                                <Link to={"#"} className="section_resultado_dados_btn">
-                                    <img src={btn_squad} alt="" />
+
+                                <Link to={""} className="section_resultado_dados_btn"><img src={btn_squad}
+                                    alt=""/>
                                 </Link>
                             </div>
                         </div>
                     </div>
+
                 </form>
+
+
             </section>
-        </main>
+
+        </main >
     );
 }
 
